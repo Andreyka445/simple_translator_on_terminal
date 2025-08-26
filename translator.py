6fadb9da9b413f15f7db312e55a82de733f50370
Andreyka445 <seleznevandrey186@outlook.com>
2025-08-26 20:06:07 +0700

    Update translator.py

--- translator.py
+++ translator.py
@@ -3,4 +3,26 @@ from googletrans import Translator
 def main():
     print("🐍 самый простой переводчик")
     print("чтобы выйти напиши 'exit'")
+
+translator = Translator()
+    
+    while True:
+        text = input("\nсюда текст, который хочешь перевести: ").strip()
+        
+        if text.lower() in ['exit', 'выход', 'quit']:
+            print("пака! 👋")
+            break
+            
+            if not text:
+            continue
+            
+        try:
+            result = translator.translate(text, dest='ru')
+            print(f"📝 перевод: {result.text}")
+            
+        except Exception as e:
+            print(f"❌ еррор: {e}")
+
+if __name__ == "__main__":
+    main()
   
